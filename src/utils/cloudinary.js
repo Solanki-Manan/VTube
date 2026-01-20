9fcdb9b691fe7ec63019b93503b9a643b1ceaed3
Solanki-Manan <solankimanan4482@gmail.com>
2026-01-20 22:39:35 +0530

    11th

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+
+const uploadoncloudinartary=async(localfilePath)=>{
+    try{
+        if(!localfilePath){
+            return null;
+        }
+        const response=await cloudinary.uploader.upload(localfilePath,{
+            resource_type:"auto",
+        })
+        //file has been uploaded sucessfully
+        console.log("file uploaded to cloudinary successfully",response.url);
+        return response;
+    }
+    catch(error){
+        fs.unlinkSync(localfilePath);//remove the locally saved temporary file
+        return null;
+    }
+}
+
+export {uploadoncloudinartary};
